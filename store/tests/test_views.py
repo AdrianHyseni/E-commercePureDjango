fd68c040264a33aa71cd266fc1aa4be3b7598657
Adrian Hyseni <adrijanhyseni500@gmail.com>
2022-02-14 06:48:20 +0100

    '2/14'

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,13 +1,15 @@
 from unittest import skip
-
-from django.contrib.auth.models import User
 from django.http import HttpRequest
-from django.test import Client, RequestFactory, TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
-
+from django.contrib.auth.models import User
 from store.models import Category, Product
-from store.views import all_products
+from store.views import product_all
 
+# @skip("demostrating skipping")
+# class TestSkip(TestCase):
+#     def test_skip_example(self):
+#         pass
 
 class TestViewResponse(TestCase):
     def setUp(self):
@@ -34,19 +36,27 @@ class TestViewResponse(TestCase):
 #test by HTTP REQUESTS
     def test_homepage_html(self):
         request = HttpRequest()
-        response = all_products(request)
+        response = product_alls(request)
         html = response.content.decode('utf8')
-        self.assertIn('<title>Home</title>',html)
+        self.assertIn('<title>Home</title>')
         self.assertEqual(response.status_code, 200)
 
     def test_view_function(self):
         request = self.factory.get('item/django-bbb')
-        response = all_products(request)
+        response = product_alls(request)
         html = response.content.decode('utf8')
-        self.assertIn('<title>Home</title>',html)
+        self.assertIn('<title>Home</title>')
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code,200)
 
+    def test_url_allowed_hosts(self):
+        response = self.c.get('/', HTTP_HOST='noaddress.com')
+        self.assertEqual(response.status_code, 200)
+        response = self.c.get('/', HTTP_HOST='yourdomain.com')
+        self.assertEqual(response.status_code, 200)
+            
+
+    
 
     
 
